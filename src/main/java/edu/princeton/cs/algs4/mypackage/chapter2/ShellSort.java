14e84d526efcf04997bfaca71d82ce242f041587
leiyulin <leiyulin@meituan.com>
2020-12-31 10:25:56 +0800

    feat: sort shell sort

--- src/main/java/edu/princeton/cs/algs4/mypackage/chapter2/ShellSort.java
+++ src/main/java/edu/princeton/cs/algs4/mypackage/chapter2/ShellSort.java
@@ -3,6 +3,8 @@ package edu.princeton.cs.algs4.mypackage.chapter2;
 public class ShellSort extends MySortExample {
     /**
      * 希尔排序
+     * 利用插入排序，比插入排序更快
+     * 对于大型数据，使用希尔排序的性能可能会较快
      *
      * @param a
      */
@@ -14,6 +16,7 @@ public class ShellSort extends MySortExample {
         }
         while (h >= 1) {
             for (int i = h; i < a.length; i++) {
+                //使用插入排序的功能，从索引左侧排序
                 //将a[i]插入到a[i-h], a[i-2h], a[i-3h]...之中
                 for (int j = i; j > h && less(a[j], a[j - h]); j -= h) {
                     exch(a, j, j - h);
