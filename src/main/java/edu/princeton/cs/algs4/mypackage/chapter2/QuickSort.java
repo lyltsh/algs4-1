ceb02d5f410c279934adc3abf5821feadecd98bc
leiyulin <leiyulin@meituan.com>
2021-01-03 17:51:31 +0800

    feat: sort quick sort 使用迭代的方式进行quick sort

--- src/main/java/edu/princeton/cs/algs4/mypackage/chapter2/QuickSort.java
+++ src/main/java/edu/princeton/cs/algs4/mypackage/chapter2/QuickSort.java
@@ -1,5 +1,7 @@
 package edu.princeton.cs.algs4.mypackage.chapter2;
 
+import com.sun.tools.javac.util.Assert;
+
 public class QuickSort extends MySortExample {
     /**
      * 快速排序
@@ -10,44 +12,63 @@ public class QuickSort extends MySortExample {
         quickSort(a, 0, a.length - 1);
     }
 
+    /**
+     * 先找出中间数据的坐标，再分成两个子数组，进行排序
+     * 小于mid索引的值，都小于等于mid索引的值
+     * 大于mid索引的值，都大于等于mid索引的值
+     */
     private static void quickSort(Comparable[] a, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        //将数组切分成两个部分
         int mid = partition(a, low, high);
+        //对数组左半部分进行排序
         quickSort(a, low, mid - 1);
+        //对数组右半部分进行排序
         quickSort(a, mid + 1, high);
     }
 
+    /**
+     * 查找的过程中，进行交换
+     * 小于start值的数据，左移
+     * 大于start值的数据，右移
+     */
     private static int partition(Comparable[] a, int low, int high) {
-        if (low >= high)
+        if (low >= high) {
             return low;
-        int i = low + 1;
-        int j = high;
-        Comparable target = a[low];
+        }
+        int start = low;
+        int i = low + 1, j = high;
         while (true) {
-            while (less(a[i++], target) && i < j) {
-                break;
+            while (less(a[i], a[start]) && i < high) {
+                i++;
             }
-            while (less(target, a[j--]) && j > i) {
-                break;
+            while (less(a[start], a[j]) && j > low) {
+                j--;
             }
-            exch(a, i, j);
             if (i >= j) {
                 break;
             }
+            exch(a, i, j);
         }
-        exch(a, low, i);
-        return i;
+        exch(a, start, j);
+        return j;
     }
 
     public static void main(String[] args) {
-        Double[] a = new Double[10];
+        Double[] a = new Double[100000];
         for (int i = 0; i < a.length; i++) {
             a[i] = Math.random();
         }
         System.out.println("Before Sort");
-        show(a);
+//        show(a);
+        long start = System.currentTimeMillis();
         sort(a);
-        assert isSorted(a);
+        long end = System.currentTimeMillis();
         System.out.println("After Sort");
-        show(a);
+//        show(a);
+        Assert.check(isSorted(a));
+        System.out.println("sort Duration: " + (end - start));
     }
 }
